195b87ecddeaef79ebd7daba7afc4f9e93e7e7ec
Raihana-2964 <148611059+Raihana-2964@users.noreply.github.com>
2024-05-24 18:28:54 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,1 +1,10 @@
-Zainab
+public class MultiplyExample {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        
+       
+         = a*b; //integer number to keep the result of multiplication
+        System.out.println("5*58 = " + c);
+    }
+ }
