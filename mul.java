8c5797ff26e736bacca69e6fb4d540c482a9f4cd
Raihana-2964 <zainabraihana1985@gmail.com>
2024-05-24 11:03:05 +0530

    my pullreq commit

--- mul.java
+++ mul.java
@@ -1,11 +1,6 @@
 public class MultiplyExample {
    public static void main(String[] args) {
-<<<<<<< HEAD
-       int a;
-       int b;
-=======
-       int a,b.c.d;
-      1234343
->>>>>>> 1a7f3416234908b622a61fd7a96847b1c589b61e
+       
+      
         }
 }
