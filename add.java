dc995ed80ee89deb23ddb20a5bef5df33f51f090
Raihana-2964 <zainabraihana1985@gmail.com>
2024-05-25 14:32:05 +0530

    committed

--- add.java
+++ add.java
@@ -5,10 +5,14 @@ public class MultiplyExample {
         
        
 <<<<<<< HEAD
+<<<<<<< HEAD
         = a*b; //integer number to keep the result of multiplication
 =======
          c= a*b; //integer number to keep the result of multiplication
 >>>>>>> 7d5a49afe1b9a34fdcfc923d66113d3b8fc18ee8
+=======
+        
+>>>>>>> 336f3c7efd48b1ebf7d92b4ca381b93a9257cffc
         System.out.println("5*58 = " + c);
     }
  }
