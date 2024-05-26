5244ff80744898d26d272d2de8410e17ba391f8d
Raihana-2964 <148611059+Raihana-2964@users.noreply.github.com>
2024-05-26 14:31:15 +0530

    Update add.java

--- add.java
+++ add.java
@@ -3,7 +3,7 @@ public class MultiplyExample {
         int a;
         int b;
         
-         c= a*b*C //integer number to keep the result of multiplication
+         c= a*b*d//integer number to keep the result of multiplication
         System.out.println("5*58 = " + c);
     }
  }
